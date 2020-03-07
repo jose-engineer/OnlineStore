383b7a2db557bad3e69220157468244d62ae19b7
jvelazqueznearbpo <jose.velazquez@colibrigroup.com>
2020-03-06 20:18:52 -0600

    Using Async pipe to unsuscribe FireBase
    Signed-off-by: jvelazqueznearbpo <jose.velazquez@colibrigroup.com>

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
@@ -8,10 +9,11 @@ import { AngularFireAuth } from 'angularfire2/auth';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  userFirebase: firebase.User;
+  auth$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => this.userFirebase = x);
+    // afAuth.authState.subscribe(authState => this.auth = authState);
+    this.auth$ = afAuth.authState;
    }
 
   logout() {
